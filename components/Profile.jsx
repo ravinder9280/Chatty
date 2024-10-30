8a56f6f368f404bf6943f3d1f480c2be5a8406ab
ravinder9280 <ravinder92809@gmail.com>
2024-10-30 19:05:01 +0530

    Update Profile.jsx

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Image from 'next/image';
+import Image from 'next/image'
 import PromptCard from './PromptCard';
 
 const Profile = ({ user, headline, desc, posts,handleDelete ,handleEdit}) => {
